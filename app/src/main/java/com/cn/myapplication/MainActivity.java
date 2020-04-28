c7231e282503f155dbedd5a67a2dd04a252024fc
admin_cheng <17719357702@163.com>
2020-04-28 22:23:18 +0800

    加载模式上

---
+++ app/src/main/java/com/cn/myapplication/MainActivity.java
@@ -0,0 +1,38 @@
+package com.cn.myapplication;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.annotation.Nullable;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+
+
+        //绑定第二个activity
+        Button button1=(Button) findViewById(R.id.start_button);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,SecondActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        //绑定自己的退出按钮
+        Button butClose=findViewById(R.id.main_finish);
+        butClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MainActivity.this.finish();
+            }
+        });
+    }
+
+}
---
+++ app/src/main/java/com/cn/myapplication/SecondActivity.java
@@ -0,0 +1,28 @@
+package com.cn.myapplication;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.annotation.Nullable;
+
+public class SecondActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_activity);
+
+        //按钮，绑定自己
+        Button butStart=findViewById(R.id.second_start);
+        butStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(SecondActivity.this,SecondActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
